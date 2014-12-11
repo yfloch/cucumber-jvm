aa5ce2b68201a631eda13c10e6a5a12c03b9d64a
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2014-12-11 13:28:28 +0000

    Failing test for java8 state isolation

--- java8/src/test/java/cucumber/runtime/java8/test/LambdaStepdefs.java
+++ java8/src/test/java/cucumber/runtime/java8/test/LambdaStepdefs.java
@@ -1,12 +1,21 @@
 package cucumber.runtime.java8.test;
 
+import cucumber.api.Scenario;
 import cucumber.api.java8.En;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
 
 public class LambdaStepdefs implements En {
+    private static LambdaStepdefs lastInstance;
+
     @Override
     public void defineGlue() {
+        Before((Scenario scenario) -> {
+            assertNotSame(this, lastInstance);
+            lastInstance = this;
+        });
+
         Given("I have (\\d+) cukes in my (.*)", (Integer cukes, String what) -> {
             assertEquals(42, cukes.intValue());
             assertEquals("belly", what);
--- java8/src/test/resources/cucumber/runtime/java8/test/java8.feature
+++ java8/src/test/resources/cucumber/runtime/java8/test/java8.feature
@@ -1,3 +1,6 @@
 Feature: Java8
   Scenario: use the API with Java8 style
     Given I have 42 cukes in my belly
+
+  Scenario: another scenario which should have isloated state
+    Given I have 43 cukes in my belly
